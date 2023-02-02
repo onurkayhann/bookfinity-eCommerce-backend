af4e6aab3048fee2214cb8378e023a97088b37a5
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-02 10:29:05 +0100

    fix API_URL

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const { config } = require('dotenv');
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
+const api = process.env.API_URL;
 
 app.get('/', (req, res) => {
   res.send('hello API');
 });
 
 app.listen(3000, () => {
+  console.log(api);
   console.log('server is running now at http://localhost:3000');
 });
