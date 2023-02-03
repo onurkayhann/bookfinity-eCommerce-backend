2483d9402387aec16d305145dfdf046ae91f9c7a
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-03 16:04:00 +0100

    add mongoose and connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -24,6 +25,20 @@ app.post(`${api}/books`, (req, res) => {
   console.log(newBook);
   res.send(newBook);
 });
+
+// Database connection
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'bookfinity',
+  })
+  .then(() => {
+    console.log('Database Connection is ready...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log('server is running now at http://localhost:3000');
