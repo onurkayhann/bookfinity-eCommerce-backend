107beff42cf6008bf1e2f25ef1a9cb7dbce491c0
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-03 15:39:19 +0100

    add morgan

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const { config } = require('dotenv');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 require('dotenv').config();
 
 const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-  res.send('hello API');
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get(`${api}/books`, (req, res) => {
+  const book = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+  };
+  res.send(book);
+});
+
+app.post(`${api}/books`, (req, res) => {
+  const newBook = req.body;
+  console.log(newBook);
+  res.send(newBook);
 });
 
 app.listen(3000, () => {
-  console.log(api);
   console.log('server is running now at http://localhost:3000');
 });
