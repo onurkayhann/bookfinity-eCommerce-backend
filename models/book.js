ca4296fc03b33693b5c693e5673940a0f2ecad59
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-05 21:49:48 +0100

    fix bookSchema

--- models/book.js
+++ models/book.js
@@ -1,12 +1,62 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    defult: '',
+  },
+  image: {
+    type: String,
+    defult: '',
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  genre: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
   author: String,
-  image: String,
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    defult: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
