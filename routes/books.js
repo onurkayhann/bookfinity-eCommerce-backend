d9e6fd474d3c2720b1c036a9f9e41dd9a3d4224d
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-09 20:45:37 +0100

    add update book function

--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,33 @@ const router = express.Router();
 
 // APIs
 
+// Create book
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send('Invalid Category');
+
+  const book = new Book({
+    name: req.body.name,
+    author: req.body.author,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    genre: req.body.genre,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
+  });
+
+  await book.save();
+
+  if (!book) return res.status(500).send('The book cannot be created.');
+
+  res.send(book);
+});
+
 // Get all books
 router.get(`/`, async (req, res) => {
   const bookList = await Book.find().populate('category');
@@ -28,28 +55,31 @@ router.get(`/:id`, async (req, res) => {
   res.send(book);
 });
 
-router.post(`/`, async (req, res) => {
+// Update book
+router.put('/:id', async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send('Invalid Category');
 
-  const book = new Book({
-    name: req.body.name,
-    author: req.body.author,
-    description: req.body.description,
-    richDescription: req.body.richDescription,
-    image: req.body.image,
-    genre: req.body.genre,
-    price: req.body.price,
-    category: req.body.category,
-    countInStock: req.body.countInStock,
-    rating: req.body.rating,
-    numReviews: req.body.numReviews,
-    isFeatured: req.body.isFeatured,
-  });
+  const book = await Book.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      author: req.body.author,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      genre: req.body.genre,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
 
-  await book.save();
-
-  if (!book) return res.status(500).send('The book cannot be created.');
+  if (!book) return res.status(500).send('The book cannot be updated!');
 
   res.send(book);
 });
