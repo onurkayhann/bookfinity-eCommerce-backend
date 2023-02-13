35a057540bc680df61195dfd323141a462782750
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-13 12:56:48 +0100

    add bcrypt, hash the user password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // ---- APIs ----
 
@@ -19,7 +20,7 @@ router.post(`/`, async (req, res) => {
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     street: req.body.street,
