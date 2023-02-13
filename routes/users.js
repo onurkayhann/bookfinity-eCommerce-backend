2f134e1ac9bee6b0b22d303cb7acc6ae7b953748
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-13 12:07:52 +0100

    add create user post rest api

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-// APIs
+// ---- APIs ----
+
+// Get all users
 router.get(`/`, async (req, res) => {
   const userList = await User.find();
 
@@ -10,6 +12,28 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(userList);
+});
+
+// Create user
+router.post(`/`, async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  user = await user.save();
+
+  if (!user) return res.status(400).send('the user cannot be created!');
+
+  res.send(user);
 });
 
 module.exports = router;
