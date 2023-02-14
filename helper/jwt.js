694f5fade7147359ecdbfdc93448bd339bd768fa
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-14 22:56:11 +0100

    add exclusive paths where users cant reach, only admin, using unless function. add register endpoint for users

--- helper/jwt.js
+++ helper/jwt.js
@@ -2,9 +2,21 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
   const secret = process.env.SECRET;
+  const api = process.env.API_URL;
+
   return expressJwt({
     secret,
     algorithms: ['HS256'],
+  }).unless({
+    path: [
+      {
+        url: { url: /\/api\/v1\/books(.*)/, methods: ['GET', 'OPTIONS'] },
+        url: { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+        methods: ['GET', 'OPTIONS'],
+      },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
   });
 }
 
