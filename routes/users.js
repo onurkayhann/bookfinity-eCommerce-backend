4f42a51da411b1f2cf49374e7e0d6ea92cf1c229
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-14 20:45:54 +0100

    add jwt, add endpoint for login user, using token and comparing password and hashed password for secure login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // ---- APIs ---- //
 
@@ -46,6 +47,31 @@ router.post(`/`, async (req, res) => {
   if (!user) return res.status(400).send('the user cannot be created!');
 
   res.send(user);
+});
+
+// Login user
+router.post(`/login`, async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const secret = process.env.SECRET;
+
+  if (!user) {
+    return res.status(400).send('User not found');
+  }
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id,
+      },
+      secret,
+      { expiresIn: '1d' } // token will expire in one day
+    );
+
+    res.status(200).send({ user: user.email, token: token });
+  } else {
+    return res.status(400).send('Invalid password!');
+  }
 });
 
 module.exports = router;
