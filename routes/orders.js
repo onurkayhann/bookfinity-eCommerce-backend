6dd5dc4f9dfcb49952a2a3001032c9b15d520403
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-17 11:49:23 +0100

    create endpoint for order for a user, resolve Promise, cimplement and import orderSchema into orders, tested and done with Postman

--- routes/orders.js
+++ routes/orders.js
@@ -1,8 +1,11 @@
 const { Order } = require('../models/order');
 const express = require('express');
+const { OrderItem } = require('../models/orderItem');
 const router = express.Router();
 
-// APIs
+// --- APIs --- \\
+
+// Getting all the orders
 router.get(`/`, async (req, res) => {
   const orderList = await Order.find();
 
@@ -10,6 +13,43 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(orderList);
+});
+
+// Create order
+router.post('/', async (req, res) => {
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        book: orderItem.book,
+      });
+
+      newOrderItem = await newOrderItem.save();
+
+      return newOrderItem._id;
+    })
+  );
+
+  const orderItemIdsResolved = await orderItemsIds; // To prevent Promise
+
+  let order = new Order({
+    orderItems: orderItemIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  });
+
+  order = await order.save();
+
+  if (!order) return res.status(400).send('Order cannot be created!');
+
+  res.send(order);
 });
 
 module.exports = router;
