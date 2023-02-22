4961095d114ad1b61f3da53481660d1478cc01e5
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-02-22 13:28:55 +0100

    add error handler for photo upload, only jpg, jpeg and png is allowed, tested with postman

--- routes/books.js
+++ routes/books.js
@@ -5,15 +5,31 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer'); // To upload files, for example uploading a photos of specific book
 
+// MIME type - allowing only jpg, jpeg, and png as photo upload
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
 // --- APIs --- \\
 
 // Multer API, to be able to upload photos
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads');
+    const isPhotoValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error(
+      'Invalid photo type, use only jpg, jpeg or png'
+    );
+
+    if (isPhotoValid) {
+      uploadError = null;
+    }
+    cb(uploadError, 'public/uploads');
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}-${Date.now()}.${extension}`);
   },
 });
